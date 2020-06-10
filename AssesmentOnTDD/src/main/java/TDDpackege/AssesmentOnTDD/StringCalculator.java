52e402bf4152890b36efa4a7aedc43444d2fa20c
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 10:26:27 +0530

    update to handle new line  case

--- AssesmentOnTDD/src/main/java/TDDpackege/AssesmentOnTDD/StringCalculator.java
+++ AssesmentOnTDD/src/main/java/TDDpackege/AssesmentOnTDD/StringCalculator.java
@@ -11,29 +11,16 @@ public class StringCalculator {
 		}
 		else if(numbers.length() >= 2 )
 		{
-		//	String temp = ""; 
 			int sum = 0; 
 			for(int i = 0; i < numbers.length(); i++) 
 			{ 
 				char ch = numbers.charAt(i); 
-				
-				// if current character is a digit 
 				if (Character.isDigit(ch)) 
 					sum += Integer.parseInt(String.valueOf(ch)); 
 		
 				
 			} 
-			/*numbers.replaceAll("\n", ",") ; 
-			System.out.println("numbers  " + numbers );
-			int sumOfNumber = 0  ; 
-			
-			 String[] nums = numbers.split(",");
-			 for (String num : nums) {
-				 System.out.println(num);
-	                if (!num.isEmpty()) {
-	                	sumOfNumber += Integer.parseInt(num);
-	                }
-	            }*/
+		
 			 return sum ; 
 		}
 		else
