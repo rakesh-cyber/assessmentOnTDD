6f340b9e3dc54a44093806083baaf80b927ba86a
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 10:18:48 +0530

    add method handle to new line in Sting

--- AssesmentOnTDD/src/main/java/TDDpackege/AssesmentOnTDD/StringCalculator.java
+++ AssesmentOnTDD/src/main/java/TDDpackege/AssesmentOnTDD/StringCalculator.java
@@ -11,14 +11,30 @@ public class StringCalculator {
 		}
 		else if(numbers.length() >= 2 )
 		{
+		//	String temp = ""; 
+			int sum = 0; 
+			for(int i = 0; i < numbers.length(); i++) 
+			{ 
+				char ch = numbers.charAt(i); 
+				
+				// if current character is a digit 
+				if (Character.isDigit(ch)) 
+					sum += Integer.parseInt(String.valueOf(ch)); 
+		
+				
+			} 
+			/*numbers.replaceAll("\n", ",") ; 
+			System.out.println("numbers  " + numbers );
 			int sumOfNumber = 0  ; 
+			
 			 String[] nums = numbers.split(",");
 			 for (String num : nums) {
+				 System.out.println(num);
 	                if (!num.isEmpty()) {
 	                	sumOfNumber += Integer.parseInt(num);
 	                }
-	            }
-			 return sumOfNumber ; 
+	            }*/
+			 return sum ; 
 		}
 		else
 			return -1 ; 
