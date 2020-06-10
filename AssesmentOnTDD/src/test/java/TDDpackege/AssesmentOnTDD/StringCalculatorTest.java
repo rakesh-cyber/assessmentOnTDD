ccc14a65a4f288add48f5ce0baa6d17a64aae999
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 09:17:08 +0530

    add test method for only one element

--- AssesmentOnTDD/src/test/java/TDDpackege/AssesmentOnTDD/StringCalculatorTest.java
+++ AssesmentOnTDD/src/test/java/TDDpackege/AssesmentOnTDD/StringCalculatorTest.java
@@ -12,5 +12,13 @@ public class StringCalculatorTest {
 		Assert.assertEquals(0, str.add(""));
         
     }
+	@Test
+    public  void AddStringContainOnlyOneElement() {
+		
+		StringCalculator str = new StringCalculator();
+		
+		Assert.assertEquals(1, str.add("1"));
+        
+    }
 
 }
