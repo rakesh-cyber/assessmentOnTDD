40dbd8552567a591fda981f836c26be5a412a336
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 09:41:45 +0530

    add test method String comtain new line

--- AssesmentOnTDD/src/test/java/TDDpackege/AssesmentOnTDD/StringCalculatorTest.java
+++ AssesmentOnTDD/src/test/java/TDDpackege/AssesmentOnTDD/StringCalculatorTest.java
@@ -36,5 +36,13 @@ public class StringCalculatorTest {
 		Assert.assertEquals(1+2+6+7+8, str.add("1,2,6,7,8"));
         
     }
+	@Test
+    public  void AddStringContainNewLine() {
+		
+		StringCalculator str = new StringCalculator();
+		
+		Assert.assertEquals(1+2+3, str.add("1\n2,3"));
+        
+    }
 
 }
