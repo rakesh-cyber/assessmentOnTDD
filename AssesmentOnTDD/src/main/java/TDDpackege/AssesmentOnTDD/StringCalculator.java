70838ca7fc868f4e1a946ed3a2c7076d50e13121
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 09:04:03 +0530

    then write code to achieve to pass empty String

---
+++ AssesmentOnTDD/src/main/java/TDDpackege/AssesmentOnTDD/StringCalculator.java
@@ -0,0 +1,12 @@
+package TDDpackege.AssesmentOnTDD;
+
+public class StringCalculator {
+	public static int add(String numbers)
+	{
+		if(numbers.length() == 0 )
+		return 0;
+		else
+			return 1 ; 
+	}
+
+}
